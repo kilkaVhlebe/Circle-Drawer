8ea7dcfe9ee4ac0c64694d5bca264583828b1217
tugaserya <artempoznakov990@gmail.com>
2024-01-31 22:36:15 +0300

    some refactor

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ const robot = require("robotjs");
 const screenSize = robot.getScreenSize()
 robot.setMouseDelay(50)
 
+//Circle Parameters(radius, amount of points for drawing)
 const PointsAmount = 50
-const CoordinateX = []
-const CoordinateY = []
 const r = 325
+//center of th screen
 const x0 = screenSize.width / 2
 const y0 = screenSize.height / 2
 
+const CoordinateX = []
+const CoordinateY = []
+
+//Calculating the coordinates of points
 let ArrIterator = 0;
 for(let i = 0;i<=2*Math.PI; i+=(2*Math.PI)/PointsAmount){
     CoordinateX[ArrIterator] = x0 + r * Math.cos(i)
@@ -17,8 +21,8 @@ for(let i = 0;i<=2*Math.PI; i+=(2*Math.PI)/PointsAmount){
     ArrIterator+=1;
 }
 
+//Drawing the circle
 robot.mouseToggle("down")
-
 for (let i = 0; i < PointsAmount; i++) {
     robot.moveMouse(CoordinateX[i], CoordinateY[i]);
 }
